43b09f83ac0b8de047966d5f17df9cc8220efb86
Risheel Chheda <98162095+risheelchheda@users.noreply.github.com>
2023-02-14 13:48:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const burger= document.querySelector("nav svg")
+
+burger.addEventListener("click",()=>{
+    if(burger.classList.contains("active")){
+        gsap.to(".links",{x:"100%"});
+        gsap.to(".line",{stroke:"white"});
+    }else{
+        gsap.to(".links",{x:"0%"});
+        gsap.to(".line",{stroke:"black"});
+        gsap.fromTo(".links a", {opacity:0, y:0}, {opacity:1, delay:0.25, y:20, stagger:0.25})
+        gsap.set("body",{overflow:"hidden"})
+    }
+    burger.classList.toggle("active");
+})
+
+const videos=gsap.utils.toArray(".video");
+gsap.set(videos,{opacity:0});
+
+videos.forEach(video =>{
+    ScrollTrigger.create({
+        trigger:video,
+        start: "top center",
+        end: "bottom center",
+        onEnter:()=>{
+            gsap.to(video,{opacity:1})
+            video.play()
+        },
+        onEnterBack:()=>video.play(),
+        onLeave:()=>video.pause(),
+        onLeaveBack:()=>video.pause(),
+    });
+})
